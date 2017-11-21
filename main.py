d85a6db555ec64b3f32a74e247a38ca9a5dc23da
Aprezao da massa <herculanodavi@aluno.ita.br>
2017-11-21 16:07:46 -0200

    O APRE EH MUITO FODA PQPQPQPQPQPQPQP

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ radio = Radio()
 c = Pubnub()
 while(1):
     time.sleep(2)
+    # reading sensor data and publishing it to webapp
     print('Sending sensor request...')
     if radio.sendSensorRequest("0") != 0:
         message = radio.getSensorData()
@@ -17,12 +18,12 @@ while(1):
     print('--------------------')
     print(' ')
 
+    # reading pubnub to change door status
     message = c.subscribe('act_get')
-
     if message is not None:
         if len(message[0]) > 0:
             print('Sending actuator request...')
-            print(radio.sendActuatorRequest(message[0][0]))
+            print(radio.sendActuatorRequest('0' + str(message[0][0])))
 
     print('--------------------')
     print(' ')
