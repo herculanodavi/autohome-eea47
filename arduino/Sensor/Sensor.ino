d928e2e67bad1eb22003189b38c61352347d9f1a
tessiop <tessio.perotti@gmail.com>
2017-11-21 17:16:17 -0200

    adding arduino

--- arduino/Sensor/Sensor.ino
+++ arduino/Sensor/Sensor.ino
@@ -1,5 +1,5 @@
 //SendReceive.ino
- 
+ //const uint64_t writingPipe = (0xE1E1E1E1F0LL); caga
 #include<SPI.h>
 #include<RF24.h>
 #include<string.h>
@@ -8,9 +8,13 @@
 RF24 radio(7, 8);
 
 char receivedMessage[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-//const uint64_t readingPipe = (0xABCDABCD71LL);
+
+// SENSOR SETTING
+const uint64_t readingPipe = (0xABCDABCD71LL);
 const uint64_t writingPipe = (0xF0F0F0F0E1LL);
-const uint64_t readingPipe = (0xA8A8E1F0C6LL);
+
+// ACTUATOR SETTING
+//const uint64_t readingPipe = (0xA8A8E1F0C6LL);
 
 
 void initRadio(){
@@ -32,19 +36,22 @@ void initRadio(){
 
 void getData(){
   radio.startListening();
-  delay(250);
+  delay(20);
   int i = 1;
   while(!radio.available()){
     delay(10);
     i++;
     if(i>=100){
-      Serial.println("getData timed out");
+      //Serial.println("getData timed out");
       return;
     }
   }
   radio.read(receivedMessage, sizeof(receivedMessage));
   Serial.print("Received message: ");
-  Serial.println(receivedMessage);
+  for(int i =0; i < 32; i++)
+    Serial.print(receivedMessage[i]);
+  Serial.println("");
+  //Serial.println(receivedMessage);
   radio.stopListening();
   delay(10);
 }
@@ -63,19 +70,4 @@ void setup(void){
   initRadio();
 }
 
-const char text[] = "1234";
-
-int analogSense() {
-  return analogRead(1);
-}
- 
-void loop(void){
-  getData();
-  int b;
-  //String text = "12345678901234567890123456789012";
-  delay(200);
-  if(receivedMessage[0] == '0'){
-    b = radio.write(text, sizeof(text));
-    Serial.println(b);
-  }
-}
+const char text[] = "SENSE";
