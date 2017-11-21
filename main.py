d4d75e50a837d2f5128f174e7e3e7695168ff3fb
tessiop <tessio.perotti@gmail.com>
2017-11-21 16:02:36 -0200

    Ready to test actuator

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 radio = Radio()
 
 c = Pubnub()
-
 while(1):
     time.sleep(2)
     print('Sending sensor request...')
@@ -14,6 +13,16 @@ while(1):
         if message is not None:
             print('Sending ' + message + ' to pubnub')
             c.publish(message, 'sen_res')
+
+    print('--------------------')
+    print(' ')
+
+    message = c.subscribe('act_get')
+
+    if message is not None:
+        if len(message[0]) > 0:
+            print('Sending actuator request...')
+            print(radio.sendActuatorRequest(message[0][0]))
 
     print('--------------------')
     print(' ')
